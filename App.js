51cbba4212e01668fe794531c2dce17de9de2919
EktaMerai <ektamerai83@gmail.com>
2024-02-11 01:12:21 +0530

    Ep-02 parcel and install react

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},[
 React.createElement("div",{id: "child"},[
-    React.createElement("h1",{},"I'm child h1 tag"),
-    React.createElement("h2",{},"I'm child h2 tag")
+    React.createElement("h1",{},"This is namaste react"),
+    React.createElement("h2",{},"Heyyy")
 ]),
 React.createElement("div",{id: "child2"},[
     React.createElement("h1",{},"I'm child h1 tag"),
@@ -9,10 +12,10 @@ React.createElement("div",{id: "child2"},[
 ])
 ]);
 
-const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
+ const heading = React.createElement("h1",{id:"heading"},"Hello world from react");
 
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 
 root1.render(parent);
 
-//{} onject we can pass attribute to h1 tag
+//{} object we can pass attribute to h1 tag
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React 
+
+# parcel
+    - Dev Build
+    - Local server
+    - HMR = Hot module replacemnet
+    - File watching algorithan and written in C++
+    - Caching - Faster builds
+    - Image optimization
+    - Bundle of files
+    - compressing file
+    - Consistent hashing
+    - Code splitting
+    - Differential bundeling - support older browser
+    - Diagnosric
+    - Error Handeling
+    - HTTPS
+    - Tree shaking - removed unused code
+    - go to parceljs.org
+    - different dev and prod bundle
+    -  
